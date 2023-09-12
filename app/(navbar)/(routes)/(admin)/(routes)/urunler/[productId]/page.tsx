ede47e58f02196416e3cbad315892fcbd0a01de2
beerloyuz <131187411+beerloyuz@users.noreply.github.com>
2023-09-12 21:59:27 +0300

    fix12

--- app/(navbar)/(routes)/(admin)/(routes)/urunler/[productId]/page.tsx
+++ app/(navbar)/(routes)/(admin)/(routes)/urunler/[productId]/page.tsx
@@ -24,7 +24,7 @@ const Page = async ({ params }: { params: { productId: string } }) => {
             <Unauthorized />
         )
     }
-    if (!product) {
+    if (!product || product === null) {
         return (
             <div className="w-full h-screen flex items-center justify-center">
                 <span className="font-light">Böyle bir ürün bulunmamaktadır.</span>
